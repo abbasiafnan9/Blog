29361ebbafbddc1ea9f5678e0d4df79d4b1779a2
Muhammad Afnan Abbasi <abbasiafnan9@gmail.com>
2021-11-03 10:37:13 -0700

    adding boiler plate

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const allRoutes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.store);
+
+// Setup express app
+
+const app = express();
+const PORT = process.env.Port || 3302;
+
+// Requiring models for syncing
+const {User} = require('./models');
+
+const sess = {
+    secret: 'Super secret',
+    cookie: {},
+    resave: false,
+    saveUninitialize: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// Setsup express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Static directory
+app.use(express.static('public'));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use('/',allRoutes);
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+sequelize.sync({force: true}).then(function(){
+    app.listen(PORT, function(){
+        console.log('App listening on PORT' + PORT);
+    });
+});
+
